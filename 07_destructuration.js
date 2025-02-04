13089a13b820516873ca76e229823bee0a12a977
Alice Sebego <alice.etvotrecom@gmail.com>
2025-02-04 12:10:24 +0100

    do exercice 7

--- 07_destructuration.js
+++ 07_destructuration.js
@@ -12,7 +12,9 @@
  *  - interdiction d'utiliser slice ou splice
  */
 
-const extractFirstTwo = () => {}
+const extractFirstTwo = ([first, second]) => [first, second];
+// console.log(extractFirstTwo([1, 2, 3])); 
+// console.log(extractFirstTwo(["Loulou", "Riri", "Fifi"]));
 
 /**
  * utilisez la décomposition pour extraire la premiere case du tableau et le reste du tableau
@@ -21,7 +23,8 @@ const extractFirstTwo = () => {}
  * exemple: [1, 2, 3] => [2, 3]
  */
 
-const extractRest = () => {}
+const extractRest = ([, ...rest]) => rest;
+//console.log(extractRest([1, 2, 3]));
 
 /**
  * utilisez la décomposition pour extraire le champ "name" de l'objet passé en paramètre
@@ -35,7 +38,8 @@ const extractRest = () => {}
  * - interdiction d'utiliser l'opérateur "." pour accéder au champ "name"
  */
 
-const extractName = () => {}
+const extractName = ({ name }) => name;
+//console.log(extractName({ name: "Andy", age: 36 }));
 
 /**
  * utilisez la décomposition pour retourner l'objet utilisateur sans le champ "password"
@@ -48,6 +52,7 @@ const extractName = () => {}
  * 
  */
 
-const removePassword = () => {};
+const removePassword = ({ password, ...user }) => user;
+//console.log(removePassword({ name: "toto", password: "1234" }));
 
 module.exports = {extractFirstTwo, extractRest, extractName, removePassword}
