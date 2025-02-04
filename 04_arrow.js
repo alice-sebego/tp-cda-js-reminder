ed09f08be62d09bbdf2fda5d151a7425d527de7f
Alice Sebego <alice.etvotrecom@gmail.com>
2025-02-04 10:25:13 +0100

    do exercices 4

--- 04_arrow.js
+++ 04_arrow.js
@@ -7,7 +7,9 @@
  *   - le mot clée "function" est interdis
  */
 
-let arrow1 = () => {}
+let arrow1 = (arg) => {
+    return arg + 1;
+}
 
 /**
  * Déclarer une fonction fléchée qui renvoi l'argument + 1
@@ -18,6 +20,9 @@ let arrow1 = () => {}
  *   - le mot clée "return" est interdis
  */
 
-let arrow2 = () => {}
+let arrow2 = (arg) => arg + 1;
+
+console.log(arrow1(6));
+console.log(arrow2(7));
 
 module.exports = {arrow1, arrow2};
