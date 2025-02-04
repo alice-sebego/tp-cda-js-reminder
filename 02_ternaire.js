a866972e3fdbf1db8a6a74762c33b4c8b8ea2b5f
Alice Sebego <alice.etvotrecom@gmail.com>
2025-02-04 10:11:58 +0100

    do exercices 2 and 3

--- 02_ternaire.js
+++ 02_ternaire.js
@@ -6,8 +6,11 @@
  *    - utiliser l'opérateur ternaire (if interdit)
  */
 function ternaire(a , b) {
+    return a > b ? a : b;
 }
 
-console.log(ternaire(1, 2));
+// console.log(ternaire(1, 2));
+// console.log(ternaire(12, 8));
+// console.log(ternaire(12, 12));
 
 module.exports = ternaire;
--- 03_ternaireChain.js
+++ 03_ternaireChain.js
@@ -1,16 +1,22 @@
-
 /**
  * Utiliser les ternaires en les chainants, pour éviter les if elseif else
- * 
+ *
  * Renvoyer ">" si a supérieur à b
  * Renvoyer "<" si a inférieur à b
  * Renvoyer "=" si a égal à b
- * 
+ *
  * Contraintes:
  *    - utiliser l'opérateur ternaire (if interdit)
  */
 
 function ternaryChain(a, b) {
+  return a > b ? ">" : 
+         a < b ? "<" : "=";
 }
+
+// console.log(ternaryChain(8, 14));
+// console.log(ternaryChain(7, 4));
+// console.log(ternaryChain(33, 99));
+// console.log(ternaryChain(9, 9));
 
 module.exports = ternaryChain;
